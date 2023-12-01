d44ab4e8913530cc5f25ad0fa5c6ada8565335c9
msugar <2496555+msugar@users.noreply.github.com>
2023-12-01 00:22:13 -0500

    Add data from billing account

--- terraform/prerequisites/main.tf
+++ terraform/prerequisites/main.tf
@@ -22,41 +22,45 @@ provider "google" {
 }
 
 resource "random_integer" "random_project_id" {
-  min = 10000
-  max = 99999
+  min = 100000
+  max = 999999
 
   keepers = {
-    project_name = "${var.project_prefix}-${var.env}"
+    project_prefix = var.project_prefix
   }
+}
+
+data "google_billing_account" "acct" {
+  display_name = "My Billing Account"
+  open         = true
 }
 
 resource "google_project" "core" {
   name       = "${var.project_prefix}-${var.env}"
   project_id = "${var.project_prefix}-${var.env}-${random_integer.random_project_id.result}"
 
+  billing_account     = data.google_billing_account.acct.id
   auto_create_network = false
 }
 
+/*
 resource "time_sleep" "wait_30_seconds" {
-  depends_on = [google_project.core]
-
+  depends_on      = [google_project.core]
   create_duration = "30s"
 }
+*/
 
 resource "google_project_service" "services" {
-  for_each   = toset(var.gcp_services_list)
-  project    = google_project.core.project_id
-  service    = each.value
-  depends_on = [time_sleep.wait_30_seconds]
+  for_each = toset(var.gcp_services_list)
+  project  = google_project.core.project_id
+  service  = each.value
+  //depends_on = [time_sleep.wait_30_seconds]
 }
 
-resource "google_storage_bucket" "tf-state" {
+resource "google_storage_bucket" "core_tf_state" {
   name     = "tf-state-${google_project.core.project_id}"
+  project  = google_project.core.project_id
   location = "NORTHAMERICA-NORTHEAST1"
 
   public_access_prevention = "enforced"
-}
-
-output "project_id" {
-  value = google_project.core.project_id
 }
